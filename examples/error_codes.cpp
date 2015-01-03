49fead82c3513082dc58a12f0b3238da4d27bd14
Vallentin <mail@vallentinsource.com>
2015-01-03 22:37:52 +0100

    Create error_codes.cpp

---
+++ examples/error_codes.cpp
@@ -0,0 +1,49 @@
+
+// Date Created: January 03, 2014
+// Last Modified: January 03, 2014
+// 
+// Author: Vallentin <mail@vallentinsource.com>
+// 
+// Github: https://github.com/VallentinSource/SimpleBMP
+
+
+#define SIMPLEBMP_OPENGL
+#include "simplebmp\simplebmp.h"
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+
+int main(int argc, char *argv[])
+{
+	SimpleBMP bmp;
+
+	const int error = bmp.load("test.bmp"); // or bmp.save("test.bmp")
+
+	switch (error)
+	{
+	case SIMPLEBMP_NO_ERROR:
+		printf("SimpleBMP: No Error");
+		break;
+
+	case SIMPLEBMP_FOPEN_ERROR:
+		printf("SimpleBMP: fopen() Error");
+		break;
+
+	case SIMPLEBMP_INVALID_SIGNATURE:
+		printf("SimpleBMP: Invalid Signature (Must be \"BM\")");
+		break;
+
+	case SIMPLEBMP_INVALID_BITS_PER_PIXEL:
+		printf("SimpleBMP: Invalid Bits Per Pixel (Must be 24 or 32)");
+		break;
+
+	default:
+		printf("SimpleBMP: Unknown Error");
+		break;
+	}
+	
+	getchar();
+    
+	return 0;
+}
