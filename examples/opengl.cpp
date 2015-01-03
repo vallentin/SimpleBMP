6a9459a12917af7ca004c6ffabc2f3e7fb9da1c6
Vallentin <mail@vallentinsource.com>
2015-01-03 22:29:57 +0100

    Create opengl.cpp

---
+++ examples/opengl.cpp
@@ -0,0 +1,26 @@
+
+// Date Created: January 03, 2014
+// Last Modified: January 03, 2014
+// 
+// Author: Vallentin <mail@vallentinsource.com>
+// 
+// Github: https://github.com/VallentinSource/SimpleBMP
+
+
+#define SIMPLEBMP_OPENGL
+#include "simplebmp\simplebmp.h"
+
+
+int main(int argc, char *argv[])
+{
+    SimpleBMP bmp;
+    bmp.load("texture.bmp");
+    
+    GLuint texture_handle;
+    glGenTextures(1, &texture_handle);
+    glBindTexture(GL_TEXTURE_2D, texture_handle);
+
+    bmp.glTexImage2D();
+    
+	return 0;
+}
