bfe137790baf11783af8995c12013491cc5a275b
Vallentin <mail@vallentinsource.com>
2015-01-05 15:41:56 +0100

    Create opengl_save_screenshot.cpp

---
+++ examples/opengl_save_screenshot.cpp
@@ -0,0 +1,45 @@
+
+// Date Created: January 05, 2014
+// Last Modified: January 05, 2014
+// 
+// Author: Vallentin <mail@vallentinsource.com>
+// 
+// Github: https://github.com/VallentinSource/SimpleBMP
+
+
+#define SIMPLEBMP_OPENGL
+#include "simplebmp\simplebmp.h"
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+
+int main(int argc, char *argv[])
+{
+    // You can't run this example just as it is
+    // as you need to initialize OpenGL, etc.
+
+	const char *path = "screenshot.bmp";
+
+	// The width and the height, would be the width
+	// and height of your current scene.
+	const int width = 1280;
+	const int height = 720;
+
+	SimpleBMP bmp(width, height);
+
+	bmp.glReadPixels();
+
+	if (!bmp.save(path))
+	{
+		printf("Successfully Saved! <%s>\n", path);
+	}
+	else
+	{
+		printf("Save Failed! <%s>\n", path);
+	}
+	
+	getchar();
+    
+	return 0;
+}
